71bbd31b8adaee474d63b68ab40962a3face5c6f
Dan Grimes <mrdangrimes@gmail.com>
2026-02-10 18:46:23 +0000

    Also, this is supposed to work even when offline. Then update when back

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,6 @@
 import {initializeApp, getApps, FirebaseApp} from 'firebase/app';
 import {getAuth, Auth} from 'firebase/auth';
-import {getFirestore, Firestore} from 'firebase/firestore';
+import {getFirestore, Firestore, enableIndexedDbPersistence} from 'firebase/firestore';
 
 import {firebaseConfig} from './config';
 export {useCollection} from './firestore/use-collection';
@@ -16,6 +16,18 @@ export function initializeFirebase() {
     firebaseApp = initializeApp(firebaseConfig);
     auth = getAuth(firebaseApp);
     firestore = getFirestore(firebaseApp);
+
+    enableIndexedDbPersistence(firestore).catch(err => {
+      if (err.code == 'failed-precondition') {
+        // This can happen if multiple tabs are open. Persistence will still work in the primary tab.
+        console.warn(
+          'Firebase persistence failed to enable in this tab. It may be active in another tab.'
+        );
+      } else if (err.code == 'unimplemented') {
+        // The browser does not support persistence.
+        console.warn('Firebase persistence is not supported in this browser.');
+      }
+    });
   } else {
     firebaseApp = getApps()[0];
     auth = getAuth(firebaseApp);
