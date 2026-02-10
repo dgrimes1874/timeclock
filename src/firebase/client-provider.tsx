ef6ff55c00fce75df69935ccb1b7689d5aa325e9
Dan Grimes <mrdangrimes@gmail.com>
2026-02-10 19:07:20 +0000

    File changes

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -6,6 +6,7 @@ import { initializeFirebase } from '.';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 
 export function FirebaseClientProvider({ children }: { children: React.ReactNode }) {
+  // Directly initialize Firebase. The component ensures this only runs on the client.
   const { firebaseApp, auth, firestore } = initializeFirebase();
 
   return (
