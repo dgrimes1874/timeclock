b8d86cbd50c9c5258455e314506a80d8f7ae2fc9
Dan Grimes <mrdangrimes@gmail.com>
2026-02-10 19:28:26 +0000

    Upload a photo or copy of your most recently used payment method   what

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -5,6 +5,8 @@ import { FirebaseProvider } from './provider';
 import { initializeFirebase } from '.';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 
+// This component now solely focuses on initializing Firebase on the client
+// and setting up the provider and error listener.
 export function FirebaseClientProvider({ children }: { children: React.ReactNode }) {
   const { firebaseApp, auth, firestore } = initializeFirebase();
 
