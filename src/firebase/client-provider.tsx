d5a451654e25284dd7a311336282a3b35dd34fe0
Dan Grimes <mrdangrimes@gmail.com>
2026-02-10 19:04:31 +0000

    Failed to publish app Something went wrong creating your App Hosting rol

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import React, { useMemo } from 'react';
+import React from 'react';
 import { FirebaseProvider } from './provider';
 import { initializeFirebase } from '.';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 
 export function FirebaseClientProvider({ children }: { children: React.ReactNode }) {
-  const firebaseApp = useMemo(() => initializeFirebase(), []);
+  const { firebaseApp, auth, firestore } = initializeFirebase();
 
   return (
-    <FirebaseProvider {...firebaseApp}>
+    <FirebaseProvider firebaseApp={firebaseApp} auth={auth} firestore={firestore}>
       {children}
       <FirebaseErrorListener />
     </FirebaseProvider>
