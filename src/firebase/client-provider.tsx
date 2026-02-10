cfd8de7277631adfd90ebac75d8db23afcb599f2
Dan Grimes <mrdangrimes@gmail.com>
2026-02-10 18:55:54 +0000

    Failed to publish app Something went wrong creating your App Hosting rol

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,44 +1,16 @@
 'use client';
 
-import React, { useEffect } from 'react';
-import { FirebaseProvider, useFirestore } from './provider';
+import React, { useMemo } from 'react';
+import { FirebaseProvider } from './provider';
 import { initializeFirebase } from '.';
-import { enableIndexedDbPersistence } from 'firebase/firestore';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 
-const firebaseApp = initializeFirebase();
-
-function OfflinePersistenceProvider({ children }: { children: React.ReactNode }) {
-    const firestore = useFirestore();
-
-    useEffect(() => {
-        const enablePersistence = async () => {
-            try {
-                await enableIndexedDbPersistence(firestore);
-            } catch (err: any) {
-                if (err.code === 'failed-precondition') {
-                    console.warn(
-                        'Firebase persistence failed to enable. It may be active in another tab.'
-                    );
-                } else if (err.code === 'unimplemented') {
-                    console.warn('Firebase persistence is not supported in this browser.');
-                }
-            }
-        };
-
-        enablePersistence();
-    }, [firestore]);
-
-    return <>{children}</>;
-}
-
-
 export function FirebaseClientProvider({ children }: { children: React.ReactNode }) {
+  const firebaseApp = useMemo(() => initializeFirebase(), []);
+
   return (
     <FirebaseProvider {...firebaseApp}>
-      <OfflinePersistenceProvider>
-        {children}
-      </OfflinePersistenceProvider>
+      {children}
       <FirebaseErrorListener />
     </FirebaseProvider>
   );
